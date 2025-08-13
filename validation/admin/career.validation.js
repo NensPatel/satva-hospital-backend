c13afbdc204b24c1b8ba50f7cf317b50bc54e858
Nens Patel <patelnens512@gmail.com>
2025-08-13 15:37:34 +0530

    Update career.validation.js

--- validation/admin/career.validation.js
+++ validation/admin/career.validation.js
@@ -4,9 +4,12 @@ export const validateCreate = async (req, res, next) => {
   const schema = Joi.object({
     name: Joi.string().min(2).required(),
     email: Joi.string().email().required(),
-    contactNo: Joi.string()
-      .pattern(/^\d{10}$/)
-      .required(),
+    contactNo: Joi.string().length(10).pattern(/^\d+$/).required().messages({
+      "string.empty": "Contact number is required",
+      "string.length": "Contact number must be exactly 10 digits",
+      "string.pattern.base": "Contact number must contain only numbers",
+    }),
+
     experience: Joi.string().required(),
     subject: Joi.string().required(),
     message: Joi.string().allow("").optional(),
