26ef3bf3769d219042b7cb385c94860c82a7db84
Nens Patel <patelnens512@gmail.com>
2025-08-18 13:54:33 +0530

    blood donation api chnages

--- routes/admin/bloodDonation.route.js
+++ routes/admin/bloodDonation.route.js
@@ -17,7 +17,7 @@ import {
 import { verifyTokenAdmin } from "../../middleware/admin/admin.auth.js";
 import { uploadBloodDonationProof } from "../../middleware/admin/upload.js";
 
-router.post("/createBloodDonation", verifyTokenAdmin, uploadBloodDonationProof, validateCreate, createBloodDonation);
+router.post("/createBloodDonation",  uploadBloodDonationProof, validateCreate, createBloodDonation);
 router.delete("/deleteBloodDonation", verifyTokenAdmin, deleteBloodDonation);
 router.post("/getPaginationData", verifyTokenAdmin, getPaginationData);
 router.post("/getDataById", verifyTokenAdmin, getDataById);
--- validation/admin/bloodDonation.validation.js
+++ validation/admin/bloodDonation.validation.js
@@ -1,74 +1,80 @@
-import Joi from 'joi';
+import Joi from "joi";
 
 export const validateCreate = async (req, res, next) => {
   const schema = Joi.object({
-  // Step 1: Personal Info
-  firstName: Joi.string().required(),
-  middleName: Joi.string().allow(''),
-  lastName: Joi.string().required(),
-  dateOfBirth: Joi.date().required(),
-  age: Joi.number(),
-  gender: Joi.string().valid('Male', 'Female', 'Other').required(),
-  email: Joi.string().email().required(),
-  mobileNumber: Joi.string().pattern(/^\d{10}$/).required(),
-  phoneNumber: Joi.string().allow(''),
-  proofType: Joi.string().allow(''),
-  proofFile: Joi.string().allow(''),
+    // Step 1: Personal Info
+    firstName: Joi.string().required(),
+    middleName: Joi.string().allow(""),
+    lastName: Joi.string().required(),
+    dateOfBirth: Joi.date().required(),
+    age: Joi.number(),
+    gender: Joi.string().valid("Male", "Female", "Other").required(),
+    email: Joi.string().email().required(),
+    mobileNumber: Joi.string().length(10).pattern(/^\d+$/).required().messages({
+      "string.empty": "Contact number is required",
+      "string.length": "Contact number must be exactly 10 digits",
+      "string.pattern.base": "Contact number must contain only numbers",
+    }),
+    phoneNumber: Joi.string().allow(""),
+    proofType: Joi.string().allow(""),
+    proofFile: Joi.string().allow(""),
 
-  // Step 2: Contact Details
-  addressLine1: Joi.string().required(),
-  addressLine2: Joi.string().allow(''),
-  district: Joi.string().required(),
-  pinCode: Joi.string().pattern(/^\d{6}$/).required(),
-  preferredContactMethod: Joi.string().required(),
-  specialInstructions: Joi.object({
-    dnd: Joi.boolean(),
-    contactAtNight: Joi.boolean(),
-    duringOfficialHours: Joi.boolean(),
-    manualTimings: Joi.boolean(),
-    other: Joi.string().allow('')
-  }),
+    // Step 2: Contact Details
+    addressLine1: Joi.string().required(),
+    addressLine2: Joi.string().allow(""),
+    district: Joi.string().required(),
+    pinCode: Joi.string()
+      .pattern(/^\d{6}$/)
+      .required(),
+    preferredContactMethod: Joi.string().required(),
+    specialInstructions: Joi.object({
+      dnd: Joi.boolean(),
+      contactAtNight: Joi.boolean(),
+      duringOfficialHours: Joi.boolean(),
+      manualTimings: Joi.boolean(),
+      other: Joi.string().allow(""),
+    }),
 
-  // Step 3: Medical Info
-  previousDonor: Joi.boolean(), 
-  bloodGroup: Joi.string().required(),
-  riskOfInfection: Joi.boolean(), 
-  bloodCenterAssociation: Joi.string().required(),
+    // Step 3: Medical Info
+    previousDonor: Joi.boolean(),
+    bloodGroup: Joi.string().required(),
+    riskOfInfection: Joi.boolean(),
+    bloodCenterAssociation: Joi.string().required(),
 
-  past6MonthsHistory: Joi.object({
-    unexplainedWeightLoss: Joi.boolean(),
-    repeatedDiarrhoea: Joi.boolean(),
-    swollenGlands: Joi.boolean(),
-    continuousLowGradeFever: Joi.boolean(),
-    tattooing: Joi.boolean(),
-    earPiercing: Joi.boolean(),
-    dentalExtraction: Joi.boolean()
-  }),
+    past6MonthsHistory: Joi.object({
+      unexplainedWeightLoss: Joi.boolean(),
+      repeatedDiarrhoea: Joi.boolean(),
+      swollenGlands: Joi.boolean(),
+      continuousLowGradeFever: Joi.boolean(),
+      tattooing: Joi.boolean(),
+      earPiercing: Joi.boolean(),
+      dentalExtraction: Joi.boolean(),
+    }),
 
-  chronicDiseases: Joi.object({
-    heartDisease: Joi.boolean(),
-    lungDisease: Joi.boolean(),
-    kidneyDisease: Joi.boolean(),
-    cancer: Joi.boolean(),
-    epilepsy: Joi.boolean(),
-    diabetes: Joi.boolean(),
-    tuberculosis: Joi.boolean(),
-    abnormalBleeding: Joi.boolean(),
-    hepatitis: Joi.boolean(),
-    allergies: Joi.boolean(),
-    jaundice: Joi.boolean(),
-    std: Joi.boolean(),
-    malaria: Joi.boolean(),
-    typhoid: Joi.boolean(),
-    faintingSpells: Joi.boolean()
-  }),
+    chronicDiseases: Joi.object({
+      heartDisease: Joi.boolean(),
+      lungDisease: Joi.boolean(),
+      kidneyDisease: Joi.boolean(),
+      cancer: Joi.boolean(),
+      epilepsy: Joi.boolean(),
+      diabetes: Joi.boolean(),
+      tuberculosis: Joi.boolean(),
+      abnormalBleeding: Joi.boolean(),
+      hepatitis: Joi.boolean(),
+      allergies: Joi.boolean(),
+      jaundice: Joi.boolean(),
+      std: Joi.boolean(),
+      malaria: Joi.boolean(),
+      typhoid: Joi.boolean(),
+      faintingSpells: Joi.boolean(),
+    }),
 
-  pastSurgeriesOrTransfusions: Joi.object({
-    majorSurgery: Joi.boolean(),
-    minorSurgery: Joi.boolean(),
-    bloodTransfusion: Joi.boolean()
-  })
- });
+    pastSurgeriesOrTransfusions: Joi.object({
+      majorSurgery: Joi.boolean(),
+      minorSurgery: Joi.boolean(),
+      bloodTransfusion: Joi.boolean(),
+    }),
+  });
 
   const { error } = schema.validate(req.body);
   if (error) {
@@ -76,4 +82,3 @@ export const validateCreate = async (req, res, next) => {
   }
   next();
 };
-
